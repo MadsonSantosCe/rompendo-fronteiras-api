b69091db7da35147c697a54e73148c3a1a8a215e
Madson Santos <madsonsamsung@gmail.com>
2025-04-28 11:38:42 -0300

    Implement user and auth routes with basic CRUD operations

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 import { PORT } from './config/env';
+import userRouter from './routes/user.routes';
+import authRouter from './routes/auth.routes';
 
 const app = express();
 app.use(express.json());
+
+app.use('/api/auth', authRouter);
+app.use('/api/users', userRouter);
 
 app.get('/', (_req, res) => {
   res.send({message : 'API is working!'});
