5f8f53d04fa8d440aff9affd16f634cb1153fe7a
Madson Santos <madsonsamsung@gmail.com>
2025-04-28 10:54:41 -0300

    Add dotenv configuration and update server to use environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express';
+import { PORT } from './config/env';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/', (_req, res) => {
   res.send({message : 'API is working!'});
 });
 
