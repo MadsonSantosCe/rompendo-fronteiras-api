58657c73823e36c7d52bc7d9422bb11014028d40
Madson Santos <madsonsamsung@gmail.com>
2025-04-28 15:17:56 -0300

    Remove dotenv dependency and related configuration

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,14 @@
 import express from 'express';
-import { PORT } from './config/env';
 import userRouter from './routes/user.routes';
 import authRouter from './routes/auth.routes';
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
-
-app.get('/', (_req, res) => {
-  res.send({message : 'API is working!'});
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
