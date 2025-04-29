c8813435ae6e77f7d956b04e35fdee0d9cf98e21
Madson Santos <madsonsamsung@gmail.com>
2025-04-29 11:49:28 -0300

    Refactor error middleware: improve error parameter naming and logging

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,19 +1,17 @@
-import { Prisma } from "@prisma/client";
 import { NextFunction, Request, Response } from "express";
 import { AppError } from "../utils/http.errors";
-import { PrismaError } from "../utils/prisma.errors";
 
 export function errorMiddleware(
-    err: any,
+    error: unknown,
     req: Request,
     res: Response,
     next: NextFunction
   ): void {
 
-    console.error("Error:", err);
+    console.error("Error:", error);
 
-    if (err instanceof AppError) {
-      res.status(err.status).json({ message: err.message });
+    if (error instanceof AppError) {
+      res.status(error.status).json({ message: error.message });
       return;
     }
   
