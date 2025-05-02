0f323009e460e5b86d010ac1df2feb1066260868
Madson Santos <madsonsamsung@gmail.com>
2025-05-02 14:34:07 -0300

    Add CORS support to the application and update package dependencies

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import userRouter from './routes/user.routes';
 import authRouter from './routes/auth.routes';
 import { errorMiddleware } from './middlewares/error.middleware';
+import cors from 'cors';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', userRouter);
